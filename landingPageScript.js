0e56c85c11a7ae46181efabd1ee26fddc5fc4800
Thomas <tchaidez@iastate.edu>
2020-09-27 18:00:28 -0500

    New condensed files

---
+++ landingPageScript.js
@@ -0,0 +1,31 @@
+function hideTextSprint1() {
+
+    var text = document.getElementById("Sprint-1-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+    function hideTextSprint2() {
+
+    var text = document.getElementById("Sprint-1-Items");
+
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+
+
+}
