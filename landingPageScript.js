06463c271f45440d97fbe378721608594ceda5ba
Thomas <tchaidez@iastate.edu>
2020-09-27 20:32:03 -0500

    New Sprints added

--- landingPageScript.js
+++ landingPageScript.js
@@ -32,3 +32,93 @@ function hideTextSprint1() {
 
 
 }
+
+function hideTextSprint3() {
+
+    var text = document.getElementById("Sprint-3-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint4() {
+
+    var text = document.getElementById("Sprint-4-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint5() {
+
+    var text = document.getElementById("Sprint-5-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint6() {
+
+    var text = document.getElementById("Sprint-6-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint7() {
+
+    var text = document.getElementById("Sprint-7-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint8() {
+
+    var text = document.getElementById("Sprint-8-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
